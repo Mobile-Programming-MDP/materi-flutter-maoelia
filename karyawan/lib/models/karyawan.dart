74584632f3290432aad874031d5e099c0d46f42a
Wasilah Maulia <wasilahmaulia_2327240119@mhs.mdp.ac.id>
2026-03-04 09:25:08 +0700

    update

--- karyawan/lib/models/karyawan.dart
+++ karyawan/lib/models/karyawan.dart
@@ -1,51 +1,38 @@
-import 'package:flutter/foundation.dart';
-
-class karyawan {
-}
-
 class Karyawan {
   final String nama;
-  final int umur; 
-  // dinamic bisa terima tipe data apa saja 
-  //data text bisa pakai rootbundle 
-  // kenapa ada toList, kluarnya adlh objek dr fromjson
-  final Alamat alamat; 
-  final List<String> hobi; 
+  final int umur;
+  final Alamat alamat;
+  final List<String> hobi;
 
-  Karyawan ({
-  required this.nama,
+  Karyawan({required this.nama, 
   required this.umur, 
-  required this.alamat
-  required this.hobi
-  }); 
+  required this.alamat, 
+  required this.hobi});
 
-  Factory Karyawan.fromJson(Map<String, dynamic> json){
+  factory Karyawan.fromJson(Map<String, dynamic> json){
     return Karyawan(
-      nama: json['nama'], 
-      umur: json['umur'], 
+      nama: json['nama'],
+      umur: json['umur'],
       alamat: Alamat.fromJson(json['alamat']),
-      hobi: Hobi.fromJson(json['hobi']),
-      )l 
-    }
+      hobi: List<String>.from(json['hobi'])
+    );
   }
+}
 
 class Alamat {
   final String jalan;
-  final String kota; 
-  // dinamic bisa terima tipe data apa saja 
-  final String provinsi; 
+  final String kota;
+  final String provinsi;
 
-  Alamat ({
-  required this.jalan,
+  Alamat({required this.jalan, 
   required this.kota, 
-  required this.provinsi
-  )}; 
+  required this.provinsi});
 
-  Factory Alamat.fromJson(Map<String, dynamic> json){
+  factory Alamat.fromJson(Map<String, dynamic> json){
     return Alamat(
-      jalan: json['jalan'], 
-      kota: json['kota'], 
+      jalan: json['jalan'],
+      kota: json['kota'],
       provinsi: json['provinsi']
-      )l 
-    }
+    );
   }
+}
